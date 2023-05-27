5dc983e3ed0676ca3dd2e8ea1def1fef62d04328
Bosko Kulusic <kulusicbosko@gmail.com>
2023-05-27 13:15:26 +0200

    added logo icon, started registration form

--- WebShopREST/WebContent/components/registerPage.js
+++ WebShopREST/WebContent/components/registerPage.js
@@ -6,7 +6,27 @@ Vue.component("registerPage",{
 	},
 	template: `
 	<div>
-		<label>Register here</label>
+		<h1>Register here</h1>
+		<form>
+			<label>Username</label>
+			<input type="text"><br>
+			<label>Password</label>
+			<input type="password"><br>
+			<label>Confirm password</label>
+			<input type="password"><br>
+			<label>First name</label>
+			<input type="text"><br>
+			<label>Last name</label>
+			<input type="text"><br>
+			<label>Gender</label><br>
+			<input type="radio" name="gender" value="M" id="male">
+			<label>Male</label><br>
+			<input type="radio" name="gender" value="F" id="female">
+			<label>Female</label><br>
+			<label>Date of birth</label>
+			<input type="date"><br><br>
+			<input type="submit" value="Register!">
+		</form>
 	</div>
 	`,
 	mounted(){
